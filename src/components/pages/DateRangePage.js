da5fea596e3fafd152cccec57e6a183e32bef4ad
rhitabratakarar <kararrhitabrata@gmail.com>
2023-07-19 00:04:01 +0530

    DateRangePicker added

--- src/components/pages/DateRangePage.js
+++ src/components/pages/DateRangePage.js
@@ -1,5 +1,19 @@
-import { CALENDARPAGE } from "../../variables";
+import "react-date-range/dist/styles.css"; // main style file
+import "react-date-range/dist/theme/default.css"; // theme css file
+import { DateRangePicker } from "react-date-range";
+import { useState } from "react";
 
-export default function DateRangePage({setPage})  {
-    return <div onClick={() => setPage(CALENDARPAGE)}>This is daterangepage</div>;
+export default function DateRangePage({ setPage }) {
+  const [selectionRange, setSelectionRange] = useState({
+    startDate: new Date(),
+    endDate: new Date(),
+    key: "selection",
+  });
+
+  function handleSelect(ranges) {
+    console.log(ranges);
+    setSelectionRange(ranges.selection);
+  }
+
+  return <DateRangePicker ranges={[selectionRange]} onChange={handleSelect} />;
 }
