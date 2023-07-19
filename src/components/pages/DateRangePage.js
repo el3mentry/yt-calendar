15da69f2920591d2d99eb19ac6c51cd109530bbd
rhitabratakarar <kararrhitabrata@gmail.com>
2023-07-19 23:42:13 +0530

    mui installed, search page, daterange page work in progress

--- src/components/pages/DateRangePage.js
+++ src/components/pages/DateRangePage.js
@@ -1,7 +1,9 @@
 import "react-date-range/dist/styles.css"; // main style file
 import "react-date-range/dist/theme/default.css"; // theme css file
-import { DateRangePicker } from "react-date-range";
+import { DateRangePicker, DateRange } from "react-date-range";
 import { useState } from "react";
+import { CALENDARPAGE } from "../../variables";
+import { Button } from "@mui/material";
 
 export default function DateRangePage({ setPage }) {
   const [selectionRange, setSelectionRange] = useState({
@@ -11,9 +13,35 @@ export default function DateRangePage({ setPage }) {
   });
 
   function handleSelect(ranges) {
-    console.log(ranges);
     setSelectionRange(ranges.selection);
   }
 
-  return <DateRangePicker ranges={[selectionRange]} onChange={handleSelect} />;
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        flexDirection: "column",
+        height: "100vh",
+      }}
+    >
+      <DateRange
+        ranges={[selectionRange]}
+        onChange={handleSelect}
+        maxDate={new Date()}
+        minDate={new Date("2006-01-01")}
+        months={2}
+        direction="horizontal"
+        editableDateInputs="true"
+        dateDisplayFormat="dd-MMM-yyyy"
+        preventSnapRefocus={true}
+        calendarFocus="backwards"
+      />
+      <br />
+      <Button onClick={() => setPage(CALENDARPAGE)} variant="outlined">
+        View Calendar
+      </Button>
+    </div>
+  );
 }
--- src/components/pages/HomePage.js
+++ src/components/pages/HomePage.js
@@ -1,5 +1,26 @@
+import { Button, TextField } from "@mui/material";
 import { DATERANGEPAGE } from "../../variables";
 
 export default function HomePage({ setPage }) {
-    return <div onClick={() => setPage(DATERANGEPAGE)}>This is Homepage.</div>
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "row",
+        height: "100vh",
+        width: "100vw",
+        justifyContent: "center",
+        alignItems: "center"
+      }}
+    >
+      <TextField id="outlined-basic" label="channel-id" variant="outlined" sx={{width: 1/4}}/>
+      <Button
+        onClick={() => setPage(DATERANGEPAGE)}
+        variant="contained"
+        sx={{ height: "3.4rem", ml: "1em"}}
+      >
+        Next
+      </Button>
+    </div>
+  );
 }
