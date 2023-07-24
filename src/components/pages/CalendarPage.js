92163afebddd77f334cb2c3b19d44ef422af95de
rhitabratakarar <kararrhitabrata@gmail.com>
2023-07-24 23:07:56 +0530

    yearly calendar removed

--- src/components/pages/CalendarPage.js
+++ src/components/pages/CalendarPage.js
@@ -1,7 +1,5 @@
 import Calendar from "../component/Calendar";
-// import YearlyCalendar from "../component/YearlyCalendar";
 
 export default function CalendarPage({ setPage }) {
-  // return <YearlyCalendar year={2012} />;
   return <Calendar />
 }
