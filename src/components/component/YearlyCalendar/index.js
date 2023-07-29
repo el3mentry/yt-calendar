e2dc785dba018ee0054ff231187fe3f311aa63e0
rhitabratakarar <kararrhitabrata@gmail.com>
2023-07-29 11:05:08 +0530

    bug removed where month was not rendering properly after changing from monthly view to yearly view and removed redundant code

--- src/components/component/YearlyCalendar/index.js
+++ src/components/component/YearlyCalendar/index.js
@@ -1,55 +1,22 @@
-import React, { useEffect, useState, useCallback } from "react";
+import React from "react";
 import dayjs from "dayjs";
 import isBetween from "dayjs/plugin/isBetween";
 import { isValidMonthsOption } from "./Utils";
 import Year from "./Year";
+import { YEAR } from "../../../variables";
 
 dayjs.extend(isBetween);
 
 const YearlyCalendar = ({ showNumberOfMonths, year }) => {
-  const initialMonth = 1;
-  const initialPage = 1;
   const totalCalendarMonths = 12;
   const _showNumberOfMonths = isValidMonthsOption(showNumberOfMonths)
     ? showNumberOfMonths
     : totalCalendarMonths;
-  const [monthsFrom, setMonthsFrom] = useState(initialMonth);
-  const [page, setPage] = useState(initialPage);
-
-  const totalPages = totalCalendarMonths / _showNumberOfMonths;
-
-  const goToPage = useCallback(
-    (_page) => {
-      const _monthsFrom = _page * _showNumberOfMonths - _showNumberOfMonths + 1;
-      setMonthsFrom(_monthsFrom);
-      setPage(_page);
-    },
-    [_showNumberOfMonths]
-  );
-
-  const findActivePage = useCallback(() => {
-    const now = dayjs();
-    const _month = now.month() + 1;
-    let _page = 1;
-    for (let i = 1; i <= totalPages; i++) {
-      const found = _month <= i * _showNumberOfMonths;
-      _page = i;
-      if (found) break;
-    }
-
-    goToPage(_page);
-  }, [goToPage, _showNumberOfMonths, totalPages]);
-
-  useEffect(() => {
-    if (_showNumberOfMonths !== totalCalendarMonths) {
-      findActivePage();
-    }
-  }, [findActivePage, _showNumberOfMonths, page]);
 
   const configYear = {
     showNumberOfMonths: _showNumberOfMonths,
     activeYear: year,
-    monthsFrom,
+    monthsFrom: (showNumberOfMonths === YEAR)? 1: dayjs().month() + 1,
   };
 
   const layoutClassName =
