1e7628fb4332c06ec1882ed0cb6ab1c8bc82b994
rhitabratakarar <kararrhitabrata@gmail.com>
2023-08-05 23:11:17 +0530

    added mui popover to day.js and hide the scrollbar

--- src/components/component/Day.js
+++ src/components/component/Day.js
@@ -1,3 +1,41 @@
+import * as React from "react";
+import Popover from "@mui/material/Popover";
+import Typography from "@mui/material/Typography";
+
 export default function Day({ dayValue }) {
-  return <div>{dayValue}</div>;
+  const [anchorEl, setAnchorEl] = React.useState(null);
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const open = Boolean(anchorEl);
+  const id = open ? "uploads-popover" : undefined;
+  return (
+    <div>
+      <div
+        onClick={handleClick}
+        sx={{ borderRadius: "8px" }}
+        aria-describedby={id}
+      >
+        {dayValue}
+      </div>
+      <Popover
+        id={id}
+        open={open}
+        anchorEl={anchorEl}
+        onClose={handleClose}
+        anchorOrigin={{
+          vertical: "bottom",
+          horizontal: "left",
+        }}
+      >
+        <Typography sx={{ p: 2 }}>The content of the Popover.</Typography>
+      </Popover>
+    </div>
+  );
 }
