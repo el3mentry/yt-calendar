e02b1eb93d52ada4918576bb046fbb9f67045020
rhitabratakarar <kararrhitabrata@gmail.com>
2023-09-06 22:08:01 +0530

    added monthly boundaries

--- src/components/component/MonthSelection.js
+++ src/components/component/MonthSelection.js
@@ -2,17 +2,28 @@ import { BiChevronLeft, BiChevronRight } from "react-icons/bi";
 import { IconButton } from "@mui/material";
 import Box from "@mui/material/Box";
 import { getMonthName } from "./YearlyCalendar/Utils";
+import dayjs from "dayjs";
+import React from "react";
 
 export default function MonthSelection({ date, setDate }) {
+  React.useEffect(() => {
+    if (date.month() <= 4 && date.year() === 2006)
+      setDate(() => dayjs("2006-05-01"));
+  });
+
   function changeToPreviousMonth() {
     setDate((prev) => {
-      return prev.subtract(1, "M");
+      return prev.year() === 2006 && prev.month() === 4
+        ? prev
+        : prev.subtract(1, "M");
     });
   }
 
   function changeToNextMonth() {
     setDate((prev) => {
-      return prev.add(1, "M");
+      return prev.year() === dayjs().year() && prev.month() === dayjs().month()
+        ? prev
+        : prev.add(1, "M");
     });
   }
 
@@ -37,8 +48,8 @@ export default function MonthSelection({ date, setDate }) {
         <IconButton onClick={changeToNextMonth} edge={"start"}>
           <BiChevronRight size={20} />
         </IconButton>
-        <div style={{ color: "#6d6d6d" }}>
-          {getMonthName(date.month() + 1)} {date.year()}
+        <div style={{ color: "#6d6d6d", width: "80px" }}>
+          {getMonthName(date.month() + 1).substring(0, 3)} {date.year()}
         </div>
       </Box>
     </div>
