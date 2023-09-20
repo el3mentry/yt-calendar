e1a24f69d472c5e5ca688465288d1acecadfbda7
rhitabratakarar <kararrhitabrata@gmail.com>
2023-09-20 21:35:02 +0530

    fixed data formatting algo

--- src/services/DataFormatter.js
+++ src/services/DataFormatter.js
@@ -9,21 +9,33 @@ export default class DataFormatter {
     this.FetchedData = fetchedData;
   }
 
+  // {date: [{obj}], date: [{obj}]}
+
   /**
    * This method is necessary to call to restructurize the data format for the application
    * @returns {undefined}
    */
   standardizeDataFormat() {
-    let preStructuredData = this.FetchedData.map(element => {
-      let snippets = element.items.map(item => item.snippet);
+    let preStructuredData = this.FetchedData.map((element) => {
+      let snippets = element.items.map((item) => item.snippet);
       return snippets;
     });
-    this.FormattedData = preStructuredData[0].map(obj => {
-      let publishedDate = dayjs(obj.publishedAt).format("DD-MM-YYYY").toString();
-      return {
-        [publishedDate]: new Data(obj.thumbnails.maxres, obj.title)
+
+    let fData = {};
+
+    for (let obj of preStructuredData[0]) {
+      let publishedDate = dayjs(obj.publishedAt)
+        .format("DD-MM-YYYY")
+        .toString();
+      const data = new Data(obj.thumbnails.maxres, obj.title);
+      if (!fData[publishedDate]) {
+        fData[publishedDate] = [data];
+      } else {
+        fData[publishedDate].push(data);
       }
-    });
+    }
+
+    this.FormattedData = fData;
   }
 
   /**
@@ -31,7 +43,7 @@ export default class DataFormatter {
    * @throws {Error}
    */
   get FormattedData() {
-    if(!this.#formattedData) {
+    if (!this.#formattedData) {
       throw new Error(this.#formattedData + " found in formattedData.");
     }
     return this.#formattedData;
@@ -43,8 +55,8 @@ export default class DataFormatter {
    * @throws {Error}
    */
   set FormattedData(value) {
-    if(!value) {
-      throw new Error('inappropriate value getting assigned to formattedData');
+    if (!value) {
+      throw new Error("inappropriate value getting assigned to formattedData");
     }
     this.#formattedData = value;
   }
@@ -54,7 +66,7 @@ export default class DataFormatter {
    * @throws {Error}
    */
   get FetchedData() {
-    if(!this.#fetchedData) {
+    if (!this.#fetchedData) {
       throw new Error(this.#fetchedData + " found in fetchedData.");
     }
     return this.#fetchedData;
@@ -66,8 +78,8 @@ export default class DataFormatter {
    * @throws {Error}
    */
   set FetchedData(value) {
-    if(!value) {
-      throw new Error('inappropriate value getting assigned to fetchedData');
+    if (!value) {
+      throw new Error("inappropriate value getting assigned to fetchedData");
     }
     this.#fetchedData = value;
   }
