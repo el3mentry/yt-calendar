782a88eda562fe2f4ddfc450e0f092e45c6729e4
rhitabratakarar <kararrhitabrata@gmail.com>
2023-10-01 21:27:10 +0530

    popover on clicking on summation button

--- src/components/component/TotalVids.js
+++ src/components/component/TotalVids.js
@@ -1,32 +1,66 @@
-import { useEffect, useState } from "react";
 import summation from "../../assets/summation.svg";
+import IconButton from "@mui/material/IconButton";
+import Popover from "@mui/material/Popover";
+import Typography from '@mui/material/Typography';
+import { useState } from "react";
 
 export default function TotalVids({ totalVideoCount = 0 }) {
+  const [anchorEl, setAnchorEl] = useState(null);
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const open = Boolean(anchorEl);
+  const id = open ? "summation-popover" : undefined;
+
   return (
-    <div
-      style={{
-        borderRadius: "50%",
-        backgroundColor: "#6cabdd",
-        width: "25px",
-        height: "25px",
-        display: "flex",
-        flexDirection: "column",
-        justifyContent: "center",
-      }}
-    >
-      <img
-        src={summation}
-        alt={"Total Videos"}
-        style={{
-          width: "100%",
-          height: "65%",
-          marginLeft: "0.005em",
-          cursor: "pointer",
+    <>
+      <IconButton aria-label="delete" size="small" onClick={handleClick}>
+        <div
+          style={{
+            borderRadius: "50%",
+            backgroundColor: "#6cabdd",
+            width: "25px",
+            height: "25px",
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+          }}
+        >
+          <img
+            src={summation}
+            alt={"Total Videos"}
+            style={{
+              width: "100%",
+              height: "65%",
+              marginLeft: "0.005em",
+              cursor: "pointer",
+            }}
+          />
+        </div>
+      </IconButton>
+
+      <Popover
+        id={id}
+        open={open}
+        anchorEl={anchorEl}
+        onClose={handleClose}
+        anchorOrigin={{
+          vertical: 'bottom',
+          horizontal: 'left',
         }}
-        onClick={() => {
-          alert(totalVideoCount);
+        transformOrigin={{
+          vertical: 'top',
+          horizontal: 'center',
         }}
-      />
-    </div>
+      >
+        <Typography sx={{ p: 2 }}>Total Videos = {totalVideoCount}</Typography>
+      </Popover>
+    </>
   );
 }
