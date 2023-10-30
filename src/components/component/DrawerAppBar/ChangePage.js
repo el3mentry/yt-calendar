4189e3267f9bb633e4f73395aa546ffc6ba00ace
rhitabratakarar <kararrhitabrata@gmail.com>
2023-10-30 23:28:28 +0530

    halfspin

--- src/components/component/DrawerAppBar/ChangePage.js
+++ src/components/component/DrawerAppBar/ChangePage.js
@@ -1,12 +1,12 @@
 import { DATERANGEPAGE, HOMEPAGE } from "../../../variables";
 import Box from "@mui/material/Box";
-import HomeIcon from '@mui/icons-material/Home';
-import EditCalendarIcon from '@mui/icons-material/EditCalendar';
-import IconButton from '@mui/material/IconButton';
+import HomeIcon from "@mui/icons-material/Home";
+import EditCalendarIcon from "@mui/icons-material/EditCalendar";
+import IconButton from "@mui/material/IconButton";
 import { useState } from "react";
-import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
-import Menu from '@mui/material/Menu';
-import Fade from '@mui/material/Fade';
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+import Menu from "@mui/material/Menu";
+import Fade from "@mui/material/Fade";
 
 export default function ChangePage({ setPage, direction = "row", style = {} }) {
   const [anchorEl, setAnchorEl] = useState(null);
@@ -21,40 +21,53 @@ export default function ChangePage({ setPage, direction = "row", style = {} }) {
     <Box sx={{ display: "flex", flexDirection: direction, ...style }}>
       <IconButton
         id="fade-button"
-        aria-controls={open ? 'fade-menu' : undefined}
+        className={`${open ? "halfspin" : ""}`}
+        aria-controls={open ? "fade-menu" : undefined}
         aria-haspopup="true"
-        aria-expanded={open ? 'true' : undefined}
+        aria-expanded={open ? "true" : undefined}
         onClick={handleClick}
-        sx={{marginLeft: "3px"}}
+        sx={{ marginLeft: "3px" }}
       >
-      <KeyboardArrowDownIcon color="info" />
+        <KeyboardArrowDownIcon color="info" />
       </IconButton>
       <Menu
         id="fade-menu"
         MenuListProps={{
-          'aria-labelledby': 'fade-button',
+          "aria-labelledby": "fade-button",
         }}
         anchorEl={anchorEl}
         open={open}
         onClose={handleClose}
         TransitionComponent={Fade}
       >
-      <Box sx={{display: "flex", flexDirection: "column", alignItems: "center", marginLeft: "3px"}}>
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            marginLeft: "3px",
+          }}
+        >
+          <IconButton
+            size="small"
+            sx={{ marginBottom: "8px" }}
+            onClick={() => {
+              setPage(HOMEPAGE);
+            }}
+          >
+            <HomeIcon sx={{ color: "#3365e7" }} />
+          </IconButton>
 
-        <IconButton size="small" sx={{ marginBottom: "8px"}} onClick={() => {
-          setPage(HOMEPAGE);
-        }}>
-        <HomeIcon sx={{ color: "#3365e7"}} />
-      </IconButton>
-
-      <IconButton size="small" onClick={() => {
-          setPage(DATERANGEPAGE);
-        }}>
-        <EditCalendarIcon sx={{ color: "#3365e7" }}/>
-      </IconButton>
+          <IconButton
+            size="small"
+            onClick={() => {
+              setPage(DATERANGEPAGE);
+            }}
+          >
+            <EditCalendarIcon sx={{ color: "#3365e7" }} />
+          </IconButton>
         </Box>
       </Menu>
-
     </Box>
   );
 }
